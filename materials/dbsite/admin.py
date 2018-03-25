b08dc7eb35b76fe70e285cd7531e7364024262ce
Alejandra Trejo <atr313@nyu.edu>
2018-03-25 11:06:54 +0400

    add admin site

--- materials/dbsite/admin.py
+++ materials/dbsite/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from dbsite.models import Material
 
-# Register your models here.
+admin.site.register(Material)
--- materials/materials/settings.py
+++ materials/materials/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'dbsite',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
