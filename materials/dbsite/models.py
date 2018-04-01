add8e2c3edc9e3d38b161dc412d17146c10f6135
Alejandra Trejo <atr313@nyu.edu>
2018-04-01 09:52:16 +0400

    import database from sql file

--- materials/dbsite/models.py
+++ materials/dbsite/models.py
@@ -3,5 +3,77 @@ from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 
 class Material(models.Model):
-    material_type_id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=100)
+    material_id = models.PositiveIntegerField(primary_key=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    material_type = models.ForeignKey('MaterialType', models.DO_NOTHING)
+    description = models.CharField(max_length=500, blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'material'
+
+
+class MaterialProcessing(models.Model):
+    material = models.ForeignKey(Material, models.DO_NOTHING)
+    processing = models.ForeignKey('Processing', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'material_processing'
+
+
+class MaterialProperty(models.Model):
+    material = models.ForeignKey(Material, models.DO_NOTHING)
+    property = models.ForeignKey('Property', models.DO_NOTHING)
+    property_value = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'material_property'
+
+
+class MaterialType(models.Model):
+    material_type_id = models.PositiveIntegerField(primary_key=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'material_type'
+
+
+class Processing(models.Model):
+    processing_id = models.PositiveSmallIntegerField(primary_key=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'processing'
+
+
+class Property(models.Model):
+    property_id = models.PositiveSmallIntegerField(primary_key=True)
+    property_type_id = models.PositiveSmallIntegerField()
+    name = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'property'
+
+
+class PropertyOption(models.Model):
+    property = models.ForeignKey(Property, models.DO_NOTHING)
+    state = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'property_option'
+
+
+class PropertyType(models.Model):
+    property_type_id = models.PositiveSmallIntegerField(primary_key=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'property_type'
--- materials/dbsite/urls.py
+++ materials/dbsite/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('$/', views.material, name='material'),
+    path('', views.index, name="index"),
 ]
 
-admin.site.site_header = 'NYUAD Material Database Admin Panel'
-admin.site.site_title = 'NYUAD Material Database Admin Panel'
+# admin.site.site_header = 'NYUAD Material Database Admin Panel'
+# admin.site.site_title = 'NYUAD Material Database Admin Panel'
